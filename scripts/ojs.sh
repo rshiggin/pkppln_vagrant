5bda14282b5183045e7a0cb326e4871fbe1bfc56
Dimitris Efstathiou <defstat@gmail.com>
2017-08-04 16:45:47 +0300

    installation of ojs2 via db and ojs3 and upgrade

--- scripts/ojs.sh
+++ scripts/ojs.sh
@@ -10,13 +10,48 @@ echo "FLUSH PRIVILEGES" | mysql -uroot -pojs
 cd /var/www/html
 
 # Clone the OJS repository
-git clone -b ojs-stable-3_0_2 --single-branch https://github.com/pkp/ojs
+git clone -b ojs-stable-2_4_8 --single-branch https://github.com/pkp/ojs
 cd ojs
 git submodule update --init --recursive
-cp /vagrant/ojsdata/config_OJS3.inc.php config.inc.php
+cp /vagrant/ojsdata/config_OJS2.inc.php config.inc.php
 mkdir ~/files
 chgrp -R www-data cache public ~/files config.inc.php
 chmod -R ug+w cache public ~/files config.inc.php
+
+
+# Install Composer dependencies
+# cd lib/pkp
+# curl -sS https://getcomposer.org/installer | php
+# php composer.phar update
+
+# Install OJS
+# wget -O - --post-data="adminUsername=admin&adminPassword=admin&adminPassword2=admin&adminEmail=ojs@mailinator.com&locale=en_US&additionalLocales[]=en_US&clientCharset=utf-8&connectionCharset=utf8&databaseCharset=utf8&filesDir=%2fhome%2fojs%2ffiles&encryption=sha1&databaseDriver=mysql&databaseHost=localhost&databaseUsername=ojs&databasePassword=ojs&databaseName=ojs&oaiRepositoryId=ojs2.localhost" "http://localhost/ojs/index.php/index/install/install"
+mkdir /home/ojs
+mkdir /home/ojs/files
+
+cp -ra /vagrant/ojsdata/ojsfiles/* /home/ojs/files
+cp -ra /vagrant/ojsdata/public/* /var/www/html/ojs/public
+
+mysql -uroot -pojs ojs < /vagrant/ojsdata/installationPhase248DataImport.sql
+
+# Import 
+cd /var/www/html/ojs/tools
+# php upgrade.php upgrade
+# php importExport.php NativeImportExportPlugin export test2.xml journaloftesting issues
+php importExport.php NativeImportExportPlugin import /vagrant/ojsdata/test.xml ojs admin
+
+# Installing OJS3
+echo "Installing OJS 3.x"
+cd /var/www/html
+
+# Clone the OJS repository
+git clone -b ojs-stable-3_0_2 --single-branch https://github.com/pkp/ojs ojs3
+cd ojs3
+git submodule update --init --recursive
+cp /vagrant/ojsdata/config_OJS3.inc.php config.inc.php
+mkdir ~/files3
+chgrp -R www-data cache public ~/files3 config.inc.php
+chmod -R ug+w cache public ~/files3 config.inc.php
 
 
 # Install Composer dependencies
@@ -24,14 +59,8 @@ cd lib/pkp
 curl -sS https://getcomposer.org/installer | php
 php composer.phar update
 
-# Install OJS
-# wget -O - --post-data="adminUsername=admin&adminPassword=admin&adminPassword2=admin&adminEmail=ojs@mailinator.com&locale=en_US&additionalLocales[]=en_US&clientCharset=utf-8&connectionCharset=utf8&databaseCharset=utf8&filesDir=%2fhome%2fojs%2ffiles&encryption=sha1&databaseDriver=mysql&databaseHost=localhost&databaseUsername=ojs&databasePassword=ojs&databaseName=ojs&oaiRepositoryId=ojs2.localhost" "http://localhost/ojs/index.php/index/install/install"
-
-mysql -uroot -pojs ojs < /vagrant/ojsdata/ojs.sql
-mysql -uroot -pojs ojs < /vagrant/ojsdata/ojs2.sql
-mysql -uroot -pojs ojs < /vagrant/ojsdata/ojs3.sql
-
 # Import 
-cd /var/www/html/ojs/tools
-
-php importExport.php NativeImportExportPlugin export test2.xml journaloftesting issues
+cd /var/www/html/ojs3/tools
+php upgrade.php upgrade
+php importExport.php NativeImportExportPlugin export test2.xml ojs issues
+# php importExport.php NativeImportExportPlugin import /vagrant/ojsdata/test.xml ojs admin
